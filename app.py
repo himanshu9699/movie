efe9ea93cb06c181083162bc74088fa14c9ad9a1
himanshu9699 <himanshu3792.be21@chitkara.edu.in>
2024-05-04 09:37:05 +0530

    .

--- app.py
+++ app.py
@@ -74,4 +74,6 @@ def recommendation():
     return render_template("prediction.html",movie_list=movie_list,status=status)
 if __name__ =='__main__':
     app.debug = True
+    app.host='0.0.0.0'
+    app.port=8080
     app.run()
